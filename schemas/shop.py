39522f722e6427883e8976a17be13cf68c1c2ca4
andrewantwi <loiskui485@gmail.com>
2025-03-21 16:48:26 +0000

    updates

--- schemas/shop.py
+++ schemas/shop.py
@@ -1,18 +1,17 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import Optional
 from datetime import datetime
 
-# Base schema (shared properties)
+
 class ShopIn(BaseModel):
     owner_id: int
     name: str
 
 
-# Schema for updating a shop
 class ShopUpdate(BaseModel):
     name: Optional[str] = None
 
-# Schema for returning a shop (response model)
+
 class ShopOut(ShopIn):
     id: int
     owner_id: int
